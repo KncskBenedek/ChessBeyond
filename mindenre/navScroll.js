a826874fb4b06a667da846b101dc2114135ab593
KncskBenedek <koncsikbenedek@gmail.com>
2022-05-01 17:18:23 +0200

    másik nav megjelenítés

--- mindenre/navScroll.js
+++ mindenre/navScroll.js
@@ -1,31 +1,5 @@
 window.addEventListener("load", init);
 
-
-
-
-/* function init(){
-    window.onscroll = function() {scroll()};
-    console.log(document.querySelector("header").clientHeight);
-    console.log(document.body.scrollTop);
-}
-
-function scroll(){
-    let elem = document.querySelectorAll("navbar");
-     console.log(document.querySelector("header").clientHeight);
-    console.log(document.body.scrollTop); 
-    if (document.main.scrollTop > document.querySelector("header").clientHeight) {
-        console.log("bement");
-        elem[0].style.top = "0";
-        elem[0].style.position = "fixed";
-        
-    } else {
-        console.log("bent else");
-        elem[0].style.top = "auto";
-       elem[0].style.position = "static";
-        
-    }
-    console.log("nem ment be");
-} */
 var navbar;
 var sticky;
 function init(){
@@ -35,10 +9,6 @@ function init(){
     console.log(sticky);
 
 }
-
-
-
-
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
